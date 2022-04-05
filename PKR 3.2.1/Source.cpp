468e803ee651f69db53e10c82df48048f0082099
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2022-04-05 14:27:22 +0300

    first

---
+++ PKR 3.2.1/Source.cpp
@@ -0,0 +1,202 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <iomanip>
+using namespace std;
+
+enum Spec { CS, IT, ME, PhIT, TR };
+
+string strSpec[] = { "Комп'ютернi науки", "Iнформатика", "Математика та економiка", "Фiзика та iнформатика", "Трудове навчання" };
+
+struct Student
+{
+    string lname;
+    int kurs;
+    Spec spec;
+    int physics;
+    int math;
+    int informatik;
+};
+
+void Create(Student* s, int amSt) {
+    int sp;
+    for (int i = 0; i < amSt; i++) {
+        cout << "Прiзвище: ";
+        cin >> s[i].lname;
+        cout << "Курс: ";
+        cin >> s[i].kurs;
+        cout << "Спецiальнiсть (0 - Комп'ютернi науки, 1 - Iнформатика, 2 - Математика та економiка, 3 - Фiзика та iнформатика, 4 - Трудове навчання) ";
+        cin >> sp;
+        s[i].spec = (Spec)sp;
+        cout << "Оцiнка з фiзики: ";
+        cin >> s[i].physics;
+        cout << "Оцiнка з математики: ";
+        cin >> s[i].math;
+        cout << "Оцiнка з інформатики: ";
+        cin >> s[i].informatik;
+    }
+}
+
+void Print(Student* s, int amSt)
+{
+    cout << "-----------------------------------------------------------------------------------------"
+        << endl;
+    cout << "| № |    Прiзвище   | Курс |      Спецiальнiсть     | Фiзика | Математика | Інформатика |"
+        << endl;
+    cout << "-----------------------------------------------------------------------------------------"
+        << endl;
+    for (int i = 0; i < amSt; i++)
+    {
+        cout << "| " << setw(2) << right << i + 1 << "";
+        cout << "| " << setw(14) << left << s[i].lname
+            << "| " << setw(5) << right << s[i].kurs
+            << "| " << setw(23) << left << strSpec[s[i].spec]
+            << "| " << setw(7) << s[i].physics
+            << "| " << setw(11) << right << s[i].math
+            << "| " << setw(7) << right << s[i].informatik;
+
+        cout << endl;
+    }
+    cout << "-----------------------------------------------------------------------------------------"
+        << endl;
+}
+
+
+double avgGrade(Student* s, const int i)
+{
+    double m = (s[0].physics + s[0].math + s[0].informatik) / 3;
+    m = (s[i].physics + s[i].math + s[i].informatik) / 3;
+    return m;
+}
+
+void Sort(Student* p, const int N)
+{
+    Student tmp;
+    for (int i0 = 0; i0 < N - 1; i0++) // метод "бульбашки"
+        for (int i1 = 0; i1 < N - i0 - 1; i1++)
+            if ((avgGrade(p, i1) > avgGrade(p, i1 + 1)) || (avgGrade(p, i1) == avgGrade(p, i1 + 1) && p[i1].math > p[i1 + 1].math)
+                ||
+                (p[i1].math == p[i1 + 1].math &&
+                    p[i1].lname > p[i1 + 1].lname))
+            {
+                tmp = p[i1];
+                p[i1] = p[i1 + 1];
+                p[i1 + 1] = tmp;
+            }
+}
+
+int* indexSort(Student* s, int N)
+{
+    int* P = new int[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        P[i] = i;
+    }
+    int i, j, value;
+    for (i = 1; i < N; i++)
+    {
+        value = P[i];
+        for (j = i - 1; j >= 0 && ((avgGrade(s, P[j]) > avgGrade(s, value))
+            ||
+            (avgGrade(s, P[j]) == avgGrade(s, value) &&
+                s[P[j]].math > s[value].math)
+            ||
+            (s[P[j]].math == s[value].math &&
+                s[P[j]].lname > s[value].lname));
+            j--)
+        {
+            P[j + 1] = P[j];
+        }
+        P[j + 1] = value;
+    }
+    return P;
+}
+
+int binSearch(Student* p, const int N, const string prizv, const int math)
+{ // повертає індекс знайденого елемента або -1, якщо шуканий елемент відсутній
+    int L = 0, R = N - 1, m;
+    do {
+        m = (L + R) / 2;
+        if (p[m].lname == prizv && avgGrade(p, m) == avgGrade(p, m) && p[m].math == math)
+            return m;
+        if ((p[m].lname < prizv)
+            ||
+            (p[m].lname == prizv &&
+                avgGrade(p, m) < avgGrade(p, m))
+            ||
+            avgGrade(p, m) < avgGrade(p, m) &&
+            p[m].math < math)
+        {
+            L = m + 1;
+        }
+        else
+        {
+            R = m - 1;
+        }
+    } while (L <= R);
+    return -1;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int amSt;
+    string lastName;
+    int math;
+    int avgGrade, found;
+    cout << "Кількість студентів: ";
+    cin >> amSt;
+    Student* s = new Student[amSt];
+
+    int menuItem;
+    do
+    {
+        cout << endl << endl << endl;
+        cout << "Виберіть дію:" << endl << endl;
+        cout << " [1] - введення даних з клавіатури " << endl;
+        cout << " [2] - вивід даних на екран" << endl;
+        cout << " [3] - фізичне впорядкування даних " << endl;
+        cout << " [4] - індексне впорядкування та вивід даних " << endl;
+        cout << " [5] - бінарний пошук для визначення, чи навчається студент " << endl;
+        cout << " із вказаним користувачем прізвищем на вказаній спеціальності та вказаним середнім балом." << endl;
+        cout << " [0] - вихід та завершення програми " << endl;
+        cout << "Введіть значеня: "; cin >> menuItem;
+        cout << endl << endl << endl;
+
+        switch (menuItem) {
+        case 1:
+            Create(s, amSt);
+            break;
+        case 2:
+            Print(s, amSt);
+            break;
+        case 3:
+            Sort(s, amSt);
+            break;
+        case 4:
+            indexSort(s, amSt);
+            break;
+        case 5:
+            cout << "Введіть ключі пошуку:" << endl;
+            cout << " Прізвище: "; cin >> lastName;
+            cout << endl;
+            cout << " Середній бал: " << endl; cin >> avgGrade;
+            cout << endl;
+            cout << " Оцінка з математики: " << endl; cin >> math;
+            cout << endl;
+            cin.get(); // очищуємо буфер клавіатури – бо залишаються символи
+            cin.sync(); // "кінець рядка", які не дають ввести наступний літерний рядок
+
+
+            if ((found = binSearch(s, amSt, lastName, math)) != -1)
+                cout << "Знайдено студента в позиції " << found + 1 << endl;
+            else
+                cout << "Шуканого студента не знайдено" << endl;
+            break;
+        case 0:
+            break;
+        }
+    } while (menuItem != 0);
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PKR 3.2.1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			const int i = 1;
+			Student s[i];
+
+			s[0].spec = CS;
+			s[0].physics = 4;
+			s[0].math = 4;
+			s[0].informatik = 4;
+			double ExpValue = 4;
+			double ActValue = avgGrade(s, 0);
+
+			Assert::AreEqual(ExpValue, ActValue);
+		}
+	};
+}
+
